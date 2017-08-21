cf62a07d73dafec2d696fb342526f1e865a1502e
luu-alex <luu.alex98@gmail.com>
2017-08-20 21:41:01 -0400

    editing files
    deleted unneccesary files, background.js myscript.js

--- popup.js
+++ popup.js
@@ -1,3 +1,12 @@
-function hidereposts(){
-  alert('hello');
-}
+// document.addEventListener("click", function(){
+//     document.getElementById("clickMe").innerHTML = "Hello World";
+// });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('clickMe');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        console.log('did something')
+    });
+});
